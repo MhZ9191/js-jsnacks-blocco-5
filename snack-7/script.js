c842992ff5a1efa15d8f5b64f17e0ff2a453c1bb
MhZ9191 <hegic91@gmail.com>
2026-01-21 17:04:28 +0100

    Added snack-7

--- snack-7/script.js
+++ snack-7/script.js
@@ -1,23 +1,35 @@
 const students = [
   {
     id: 1,
-    name: 'Marco Lanci',
+    name: "Marco Lanci",
     age: 32,
-    class: '3C'
+    class: "3C",
   },
   {
     id: 2,
-    name: 'Mario Banfi',
+    name: "Mario Banfi",
     age: 34,
-    class: '4A'
+    class: "4A",
   },
   {
     id: 3,
-    name: 'Luigi Banzi',
+    name: "Luigi Banzi",
     age: 33,
-    class: '5B'
+    class: "5B",
   },
 ];
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+//!
+let studentIdTwo;
+students.forEach((element) => {
+  const { id } = element;
+  if (id === 2) studentIdTwo = element;
+});
+console.log(studentIdTwo);
+
+//!
+const studenteIdDue = students.find(({ id }) => id === 2);
+console.log(studenteIdDue);
