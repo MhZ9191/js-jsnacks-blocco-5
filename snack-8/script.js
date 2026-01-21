e7c0a8f63a9a73ef84d13121d1bf856dc240eb50
MhZ9191 <hegic91@gmail.com>
2026-01-21 17:21:51 +0100

    Added snack-8

--- snack-8/script.js
+++ snack-8/script.js
@@ -1,23 +1,41 @@
 const students = [
   {
     id: 1,
-    name: 'Marco Lanci',
+    name: "Marco Lanci",
     age: 32,
-    class: '3C'
+    class: "3C",
   },
   {
     id: 2,
-    name: 'Mario Banfi',
+    name: "Mario Banfi",
     age: 34,
-    class: '4A'
+    class: "4A",
   },
   {
     id: 3,
-    name: 'Luigi Banzi',
+    name: "Luigi Banzi",
     age: 33,
-    class: '5B'
+    class: "5B",
   },
 ];
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+//!
+let myObj;
+students.forEach((element) => {
+  const { name } = element;
+  if (name === "Marco Lanci") myObj = element;
+});
+console.log(myObj.class);
+
+//!
+const marcoLanci = students.find(({ name }) => name === "Marco Lanci");
+console.log(marcoLanci.class);
+
+const { class: studentClass } = marcoLanci;
+console.log(studentClass);
+
+const myClass = marcoLanci?.class;
+console.log(myClass);
