1945dda4747947eb1f2ce96b733f4a9af7b07be9
MhZ9191 <hegic91@gmail.com>
2026-01-21 16:36:02 +0100

    Added snack-4

--- snack-4/script.js
+++ snack-4/script.js
@@ -1,9 +1,36 @@
 const posts = [
-  { author: 'Marco', date: '12/3/2021', content: 'lorem ipsum...' },
-  { author: 'Luca', date: '30/6/2021', content: 'lorem ipsum...' },
-  { author: 'Fabrizio', date: '12/9/2020', content: 'lorem ipsum...' },
-  { author: 'Enrico', date: '09/12/2021', content: 'lorem ipsum...' },
-]
+  { author: "Marco", date: "12/3/2021", content: "lorem ipsum..." },
+  { author: "Luca", date: "30/6/2021", content: "lorem ipsum..." },
+  { author: "Fabrizio", date: "12/9/2020", content: "lorem ipsum..." },
+  { author: "Enrico", date: "09/12/2021", content: "lorem ipsum..." },
+];
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+//!
+const newArray = posts.map((element) => {
+  const currentElement = element;
+  return currentElement.author;
+});
+console.table(newArray);
+
+//!
+const newArrayTwo = posts.map((element) => element.author);
+console.table(newArrayTwo);
+
+//!
+const newArrayThree = posts.map((element) => {
+  const { author } = element;
+  return author;
+});
+console.table(newArrayThree);
+
+//!
+const newArrayFour = posts.map(({ author }) => author);
+console.table(newArrayFour);
+
+//!
+const newArrayFive = [];
+posts.forEach(({ author }) => newArrayFive.push(author));
+console.table(newArrayFive);
