3853e6d9780171225ca38d45a6b24f7aa1eb5868
MhZ9191 <hegic91@gmail.com>
2026-01-21 15:56:39 +0100

    Added snack-1

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,4 +1,18 @@
-const names = ['Edoardo', 'Simone', 'Francesco'];
+const names = ["Edoardo", "Simone", "Francesco"];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+//!
+names.forEach((element) => {
+  const currentElement = element;
+  console.log(currentElement);
+});
+
+//!
+names.forEach((element) => {
+  console.log(element);
+});
+
+//!
+names.forEach((element) => console.log(element));
