25d45796f6cd5460ee3c531076625ac1ae09243e
MhZ9191 <hegic91@gmail.com>
2026-01-21 16:45:10 +0100

    Added snack-5

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,28 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+//!
+const numsEven = nums.filter((element) => {
+  const currentEl = element;
+  return element % 2 === 0;
+});
+console.table(numsEven);
+
+//!
+const numsEvenTwo = nums.filter((element) => element % 2 === 0);
+console.table(numsEvenTwo);
+
+//!
+const numsEvenThree = [];
+nums.forEach((element) => {
+  if (element % 2 == 0) numsEvenThree.push(element);
+});
+console.table(numsEvenThree);
+
+//!
+function verifyEven(num) {
+  return num % 2 === 0;
+}
+const numsEvenFour = nums.filter((element) => verifyEven(element));
+console.table(numsEvenFour);
